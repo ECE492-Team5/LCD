462929955b54038bb9bbb1810395b9dd0736b0ba
Andrei <drei3000@gmail.com>
2017-03-19 15:35:36 -0600

    calibration essentially done. need to test

--- calibration_testing.c
+++ calibration_testing.c
@@ -1,9 +1,12 @@
+#include <math.h>
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
 // #include <error.h>
 #include <stdlib.h>
 #include <sys/mman.h>
+#include "cjson/cJSON.h"
+#include "cjson/cJSON.c"
 
 //testing
 #include <time.h>
@@ -24,43 +27,54 @@
 // #define HW_REGS_SPAN ( 0x04000000 )
 // #define HW_REGS_MASK ( HW_REGS_SPAN - 1 )
 #define SAMPLE_SIZE 10
+#define FILE_CHAR_SIZE 2000
 
-// int set_current_range(int *min, int *max, char**argv);
-double get_avg_sensor_value(double *value);
+#define MIN_VOLTS "min_avg_voltage"
+#define MAX_VOLTS "max_avg_voltage"
+#define MIN_AMPS "min_amperage"
+#define MAX_AMPS "max_amperage"
+#define MULTIPLIER "multiplier"
+
+void get_avg_sensor_value(double *value);
+char *readFile();
+void writeFile(char *str);
+void init_sensor_json(cJSON **root, char *name);
+
+// JSON library link: https://github.com/DaveGamble/cJSON/tree/master
 
 int main(int argc, char**argv) {
 	//testing
-
-
 	void *base;
     uint32_t *adc_base;
     int memdevice_fd;
     int i;
-    double *value;
+    char sensor_name[20];
+    double voltage_off, voltage_on, max, multiplier, some_value;
     const int nReadNum = 10;
 
     int channel = 0x00 & 0x07;
     
     if (argc == 2) {
-    	// invalid input.
-    } else if (argc == 3) {
-    	// min/max
-    	// configure all channels
-    	// set_current_range(&min, &max, argv);
-    } else if (argc == 4) {
-    	// min/max/sensor#
-    	// set_current_range(&min, &max, argv);
-        // channel = ((uint8_t) atoi(argv[3])) & 0x07;
+        // enter the sensor value.
+        sprintf(sensor_name, "current_sensor_%i", atoi(argv[1]));
+        // channel = ((uint8_t) atoi(argv[1])) & 0x07;
     } else {
-    	// printf("Incorrect number of inputs.\n");
-    	// exit(1);
+    	printf("Incorrect number of inputs.\n");
+    	exit(1);
     }
 
+    // Read file and initialize the json object.
+    char *str = readFile();
+    cJSON *root = cJSON_Parse(str);
+    cJSON *sensor = cJSON_GetObjectItem(root, sensor_name);
 
-
-
-
-
+    // Initialize any sensors not found in json file.
+    if (sensor == NULL) {
+        init_sensor_json(&root, sensor_name);
+        sensor = cJSON_GetObjectItem(root, sensor_name);
+    }
+    cJSON_GetObjectItem(sensor, MAX_VOLTS)->valuedouble = 22.00;
+    
 /*
     // Open /dev/mem device
     if( (memdevice_fd = open("/dev/mem", (O_RDWR | O_SYNC))) < 0) {
@@ -100,44 +114,104 @@ int main(int argc, char**argv) {
     while ((*adc_base & 0x01) == 0x00);
 */
 
-    printf ("Turn off the current to the sensors. Press ANY KEY to continue. ");   
+    // calibrate when the current is off.
+    printf ("Turn off the sensors. Press ANY KEY to continue. ");  
     getchar();
-    
-    // printf("CH%d = %.3fV (0x%04x) \n", channel, (float)value/1000.0, value);
-    printf("%.3fV\n", get_avg_sensor_value(&value));
-   	// printf( "You entered: %d \n", min);
-   	// printf( "You entered: %d \n", max);
+    cJSON_GetObjectItem(sensor, MIN_AMPS)->valuedouble = 0.00;
 
+    get_avg_sensor_value(&voltage_off);
+    cJSON_GetObjectItem(sensor, MAX_VOLTS)->valuedouble = voltage_off;
+    printf("Average voltage for OFF sensors: %.2fV\n", voltage_off);
+    printf("\n");
+
+    // calibrate when the current is on.
+    printf ("Turn on the sensors. Enter max expected amperage (mA): ");   
+    scanf("%lf", &max);
+    cJSON_GetObjectItem(sensor, MAX_AMPS)->valuedouble = max;
+
+    get_avg_sensor_value(&voltage_on);
+    cJSON_GetObjectItem(sensor, MIN_VOLTS)->valuedouble = voltage_on;
+    printf("Average voltage for ON sensors: %.2fV\n", voltage_on);
+    printf("You entered %.2fmA as max.\n", max);
+    printf("\n");
+
+    // calibrate the multiplier
+    multiplier = max/fabs(voltage_off-voltage_on);
+    cJSON_GetObjectItem(sensor, MULTIPLIER)->valuedouble = multiplier;
+
+
+    printf ("Enter a voltage to map: ");   
+    scanf("%lf", &some_value);
+
+    printf("That's %.2fA.\n", (some_value-voltage_off)*multiplier);
 /*
-    // unmap and close /dev/mem 
+    // unmap and close /dev/mem
     if (munmap(base, HW_REGS_SPAN) < 0) {
         perror("munmap() failed.");
         close(memdevice_fd);
         exit(EXIT_FAILURE);
     }
 */
+
+    // write back to file and free memory
+    char *updated = cJSON_Print(root);
+    writeFile(updated);
+    cJSON_Delete(root);
+
     close(memdevice_fd);
     exit(EXIT_SUCCESS);
 }
 
-// int set_current_range(int *min, int *max, char**argv) {
-// 	*min = atoi(argv[1]);
-// 	*max = atoi(argv[2]);
-// 	if (*min > *max) {
-//     	fprintf(stderr, "max is smaller than min current.\n");
-//     	exit(0);
-// 	}
-// 	return 0;
-// }
-// 
-double get_avg_sensor_value(double *value) {
+// void get_avg_sensor_value(double *value, uint32_t **adc_base) {
+void get_avg_sensor_value(double *value) {
     srand(time(NULL)); 
     int i;
     *value = 0;
     for (i = 0; i < SAMPLE_SIZE; ++i) {
-        //testing
-        *value += rand()/100000;    
+        *value += rand()/100000;                             // comment out for running
+        // *value += *(*adc_base + 0x01);                       // comment out for testing
     }
     *value /= SAMPLE_SIZE;
-    return 0;
+}
+
+// Function from a save file.
+// Modified for our program
+// http://stackoverflow.com/questions/4823177/reading-a-file-character-by-character-in-c
+char *readFile() {
+    FILE *file = fopen("sensor_data_json.txt", "r");
+    char *code;
+    size_t n = 0;
+    int c;
+
+    if (file == NULL)
+        exit(1); //could not open file
+
+    code = malloc(FILE_CHAR_SIZE);
+
+    while ((c = fgetc(file)) != EOF) {
+        code[n++] = (char) c;
+    }
+
+    code[n] = '\0';        
+    return code;
+}
+
+void writeFile(char *str) {
+    FILE *file = fopen("sensor_data_json.txt", "w");
+    if (file != NULL) {
+        fprintf(file, "%s", str);
+        fclose(file);
+    }
+}
+
+void init_sensor_json(cJSON **root, char name[20]) {
+    cJSON *sensor;
+    cJSON_AddItemToObject(*root, name, sensor = cJSON_CreateObject());
+    cJSON_AddNumberToObject(sensor, MIN_VOLTS, 0.00);
+    cJSON_AddNumberToObject(sensor, MAX_VOLTS, 0.00);
+    cJSON_AddNumberToObject(sensor, MIN_AMPS, 0.00);
+    cJSON_AddNumberToObject(sensor, MAX_AMPS, 0.00);
+    cJSON_AddNumberToObject(sensor, MULTIPLIER, 0.00);
+
+    writeFile(cJSON_Print(*root));
 }
